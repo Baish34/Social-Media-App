2148ace65b757356db00e59b6c4524cf02f13fed
Baishnawi Agrawal <baishnawiagrawal08@gmail.com>
2024-10-30 10:52:01 +0545

    feat: update App component to include LandingPage route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import LandingPage from "./components/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
@@ -16,3 +18,4 @@ function App() {
 }
 
 export default App;
+
