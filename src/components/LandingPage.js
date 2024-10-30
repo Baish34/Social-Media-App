63f89b4661b44427852a87f343d1231f97741fb1
Baishnawi Agrawal <baishnawiagrawal08@gmail.com>
2024-10-30 10:51:15 +0545

    feat: add LandingPage component with navigation to Login and Register

---
+++ src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <div className="container text-center p-5">
+      <h1>Welcome to Our App</h1>
+      <p>Please log in or register to get started.</p>
+      <div className="mt-4">
+        <Link to="/login" className="btn btn-primary me-3">
+          Login
+        </Link>
+        <Link to="/register" className="btn btn-secondary">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
